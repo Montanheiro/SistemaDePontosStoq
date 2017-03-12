c5254f5fe4472584b533a677accf055669c5c22d
Montanheiro <lucasmontanheiro10@gmail.com>
2017-03-11 22:58:20 -0300

    PUT for update

--- resource/ClientResource.java
+++ resource/ClientResource.java
@@ -17,6 +17,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -124,6 +125,7 @@ public class ClientResource {
         return gson.toJson(client);  
     }
     
+    @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/updatepassword")
     public Boolean updatePassword(@HeaderParam("token") String token) 
